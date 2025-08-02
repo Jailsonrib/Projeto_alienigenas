543c0bcb41ae7f69cca4eeaa17536a9d42e76716
Jailson Ribeiro <jailsonribeiro7@gmail.com>
2025-08-02 20:30:31 -0300

    Add new files on the  modulo alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,8 +5,9 @@ class AlienInvasion:
     def __init__(self):
         '''Inicializa o jogo e cria recursos do jogo'''
         pygame.init()
-        self.screen = pygame.display.set_mode(1200, 800)
-        pygame.display.set_caption('Alien Invasion')
+        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Meu primeiro jogo usando o pygame')
         
     def run_game(self):
         '''inicia o loop principal do jogo'''
@@ -15,5 +16,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    #Deixa a tela desenhada mais recente visível
             pygame.display.flip()
-        
+            self.clock.tick(60)
+if __name__=="__main__":
+    #Cria uma instância do jogo e executa o jogo
+    ai = AlienInvasion()
+    ai.run_game()
