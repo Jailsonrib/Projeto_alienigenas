dedebb94a59b9c1bf357ab12f9dd5bec86447c17
Jailson Ribeiro <jailsonribeiro7@gmail.com>
2025-08-17 21:17:33 -0300

    new file:   alien.py         new file:   images/alien.png         new file:   images/alien_black.png         new file:   images/ship_recolor.png

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Classe para representar um alienígena na frota."""
+    def __init__(self, ai_game):
+        """Inicializa o alienígena e define sua posição inicial."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Carrega a imagem do alienígena e define seu retângulo
+        self.image = pygame.image.load("imagens/alien.png")
+        self.rect = self.image.get_rect()
+
+        # Inicia cada novo alienígena próximo ao canto superior esquerdo da tela
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Armazena a posição horizontal do alienígena
+        self.x = float(self.rect.x)
